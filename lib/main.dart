dfb4d1783892e8b355de5c4c66706ad793d43c52
Lucas Silva <lucassilva080397@gmail.com>
2022-10-17 11:30:22 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Bem-vindo à Política de Privacidade da Maktub Company',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Usuários que utilizam nosso serviço são muito importantes para nós. Dessa forma, quando você começar a utilizar nossos serviços, gostaríamos que ficasse sabendo como estamos usando suas informações e o modo como estamos protegendo a sua privacidade. Nossa Política de Privacidade explica que tipo de informações coletamos. Sua privacidade é importante para nós, portanto, por favor reserve um tempo para conhecer nossas práticas. E, se você tiver alguma dúvida, entre em contato conosco.',
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Informações que coletamos',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'A Maktub Company, não coleta, armazena ou divulga nenhum tipo de informação que o seu usuário, eventualmente, venha a fornecer durante o uso de nossos aplicativos. Isso quer dizer que o usuário não precisa em nenhum momento informar qualquer tipo de dados de uso pessoal.',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
